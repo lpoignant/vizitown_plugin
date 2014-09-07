aa0755d1ebdf75ce57e479b10a133143cc833429
Louis POIGNANT <poignant.louis@gmail.com>
2014-09-07 22:31:18 +0200

    improve pep8 script

--- pep8.sh
+++ pep8.sh
@@ -1,7 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
-if hash pep8-python2 2>/dev/null; then
-	pep8-python2 --show-source --max-line-length=160 --ignore=W292 --exclude=vt_test_converter.py,plugin_upload.py,resources_rc.py,ui_vizitown.py,./cyclone/*,./zope/*,./twisted/*,./help/*,__init__.py,gdal_retile.py,gdal_merge.py .
+files="vectors/*.py"
+files+=" core/*.py"
+files+=" ./*.py"
+
+param="--max-line-length=160 --ignore=E265 "
+
+./clear.sh
+
+if has pep8-python2 2>/dev/null; then
+
+	pep8-python2 $param $files
+
 else
-	pep8 --show-source --max-line-length=160 --ignore=W292 --exclude=vt_test_converter.py,plugin_upload.py,resources_rc.py,ui_vizitown.py,./cyclone/*,./zope/*,./twisted/*,./help/*,__init__.py,gdal_retile.py,gdal_merge.py .
+
+	pep8 $param $files
+
 fi
